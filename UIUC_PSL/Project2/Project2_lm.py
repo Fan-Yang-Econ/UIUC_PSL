f36b2fdcb875ed38e876992b827656643ccf5546
yafa <yafa@amazon.com>
2021-04-05 15:39:25 -0700

    tried lasso with store + dept

--- UIUC_PSL/Project2/Project2_lm.py
+++ UIUC_PSL/Project2/Project2_lm.py
@@ -1,11 +1,229 @@
 import os
 import logging
+from copy import deepcopy
+import warnings
 import datetime
+import time
+import threading
 
+from dateutil.relativedelta import relativedelta
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from dateutil.relativedelta import relativedelta
+from sklearn.linear_model import Lasso
+
+
+def start_multi_threading(thread_list,
+                          max_threads=20,
+                          all_threads_have_to_be_success=True):
+    if len(thread_list) == 0:
+        return None
+    
+    for count_i, thread in enumerate(thread_list):
+        thread.daemon = True
+        thread.start()
+        logging.info("""Thread {thread_id} started""".format(thread_id=thread.name))
+        
+        while len([thread_i for thread_i in thread_list if thread_i.is_alive()]) >= max_threads:
+            time.sleep(0.05)
+            logging.debug('You have run too many threads! Have a rest!!')
+    
+    for thread in thread_list:
+        
+        thread.join()
+        
+        if all_threads_have_to_be_success and \
+                'exitcode' in dir(thread) and \
+                thread.exitcode == 1:
+            
+            raise threading.ThreadError(f"{thread.name} failed")
+        else:
+            logging.info(f"{thread.name} Done")
+
+
+
+# data processing - change date fto datetime format and get year, month and week indicator
+# df_train['Date'] = pd.to_datetime(df_train['Date'])
+
+def encode_calender(data, max_week=None):
+    """
+    SOME HOLIDAY ALWAYS HAPPEN IN CERTAIN WEEK IN TRAINING DATA
+    THUS DELETE THE WEEKLY DUMMY THERE, AS HOLIDAY MAY HAPPEN IN ANOTHER WEEK ~~~
+
+    :param data:
+    :param max_week:
+    :return:
+    """
+    data.loc[:, 'Week'] = data['Date'].dt.isocalendar().week
+    if max_week is None:
+        max_week = data['Week'].max()
+    data.loc[:, 'Year'] = data['Date'].dt.isocalendar().year
+    data.loc[:, 'Month'] = pd.DatetimeIndex(data['Date']).month
+    # data['Week'] = data.apply(lambda x: x['Week'] - 1 if x['Year'] == 2010 else x['Week'], axis=1)
+    # dummy for holiday
+    
+    list_holiday_names = []
+    holiday_list = [2, 9, 11, 12]
+    for i, m in zip(range(1, 5), holiday_list):
+        name = 'holiday' + str(i)
+        data.loc[:, name] = (data['IsHoliday'] & (data['Month'] == m)).apply(lambda x: 1 if x else 0)
+        list_holiday_names.append(name)
+    
+    # dummy for week
+    for week_i in range(1, max_week):
+        week_name = 'wk' + str(week_i)
+        data[week_name] = (data['Week'] == week_i).apply(lambda x: 1 if x else 0)
+        for holiday_name in list_holiday_names:
+            #     SOME HOLIDAY ALWAYS HAPPEN IN CERTAIN WEEK IN TRAINING DATA
+            #     THUS DELETE THE WEEKLY DUMMY THERE, AS HOLIDAY MAY HAPPEN IN ANOTHER WEEK ~~~
+            if week_name in data and (data[holiday_name] == data[week_name]).sum() == len(data):
+                del data[week_name]
+    #
+    # for week_name in list_week_names:
+    #     for holiday_name in list_holiday_names:
+    #         holiday_week_ind = (data[holiday_name] & data[week_name])
+    #         if holiday_week_ind.sum() > 0:
+    #             data.loc[:, holiday_name + '-' + week_name] = holiday_week_ind
+    #
+    # add a trend variable
+    data['Week_count'] = data['Week'] + (data['Year'] - START_YEAR) * 52
+    
+    return data
+
+
+def encode_cate(train_, next_folder_, cate_vars=('Store', 'Dept')):
+    for cat_var_ in cate_vars:
+        for _id in set(train_[cat_var_].unique().tolist() + next_folder_[cat_var_].unique().tolist()):
+            for _df in [train_, next_folder_]:
+                _df.loc[:, f'{cat_var_}_{_id}'] = (_df[cat_var_] == _id).apply(lambda x: 1 if x else 0)
+    
+    return train_, next_folder_
+
+
+def eva_error(next_fold_, test_pred_, abs=True):
+    # extract predictions matching up to the current fold
+    scoring_df = next_fold_.merge(test_pred_, on=['Date', 'Store', 'Dept'], how='left', indicator=True)
+    
+    # extract weights and convert to numpy arrays for wae calculation
+    weights = scoring_df['IsHoliday_x'].apply(lambda is_holiday: 5 if is_holiday else 1).to_numpy()
+    actuals = scoring_df['Weekly_Sales'].to_numpy()
+    preds = scoring_df['Weekly_Pred'].fillna(0).to_numpy()
+    if abs:
+        error = np.sum(weights * np.abs(actuals - preds)) / np.sum(weights).item()
+    else:
+        error = np.sum(weights * (actuals - preds)) / np.sum(weights).item()
+    return error
+
+
+def _predict(list_storage, _df_test, _df_train, x_list, y):
+    reg = Lasso(alpha=0.001, normalize=True, max_iter=10000).fit(_df_train[x_list], y)
+    _df_test.loc[:, 'Weekly_Pred'] = reg.predict(_df_test[x_list])
+    list_storage.append(_df_test)
+
+
+def mypredict(train, test, next_fold, t, show_training_error=False, target_fold=None, thread_n=1):
+    """
+    
+    :param train:
+    :param test:
+    :param next_fold:
+    :param t:
+        t = 1
+    :param show_training_error:
+        show_training_error = True
+    :return:
+    """
+    if next_fold is None:
+        new_train = deepcopy(train)
+    else:
+        new_train = pd.concat([train, next_fold])
+        new_train = new_train.drop_duplicates(['Date', 'Store', 'Dept'])
+    
+    test_start_dt = datetime.datetime(2011, 3, 1) + relativedelta(months=2 * (t - 1))
+    test_end_dt = datetime.datetime(2011, 3, 1) + relativedelta(months=2 * t)
+    
+    new_test = test[(test['Date'] >= test_start_dt) & (test['Date'] < test_end_dt)]
+    
+    if target_fold is not None and t != target_fold:
+        new_test['Weekly_Pred'] = 0
+        return new_train, new_test[['Date', 'Store', 'Dept', 'Weekly_Pred', 'IsHoliday']]
+    
+    df_train = deepcopy(new_train)
+    df_train = encode_calender(data=df_train)
+    df_new_test = encode_calender(data=deepcopy(new_test), max_week=df_train['Week'].max())
+    
+    x_list = [col for col in set(list(df_train.columns) + list(df_new_test.columns))
+              if col not in ['Date', 'Store', 'Dept',
+                             'Weekly_Sales', 'Weekly_Sales_lag', 'diff',
+                             'IsHoliday', 'Week', 'Month']]
+    
+    # make both train and test have the same variables
+    for x in x_list:
+        if x not in df_new_test:
+            df_new_test[x] = 0
+        if x not in df_train:
+            df_train[x] = 0
+    
+    LIST_DF_PRED = []
+    list_thread = []
+    for dept, _df_one_dept in df_new_test.groupby('Dept'):
+        for store, _df in _df_one_dept.groupby('Store'):
+            logging.info(f' === Processing folder {t}: dept-{dept} and store-{store} ===')
+            # dept = 23
+            # store = 43
+            df_new_test_one_dept = _df[(_df['Dept'] == dept) & (_df['Store'] == store)]
+            df_train_one_dept = df_train[(df_train['Dept'] == dept) & (df_train['Store'] == store)]
+            
+            if df_train_one_dept.empty:
+                df_train_one_dept = df_train[(df_train['Dept'] == dept)]
+            
+            y = df_train_one_dept['Weekly_Sales']
+            if thread_n > 1:
+                _thread = threading.Thread(target=_predict,
+                                 kwargs={'list_storage': LIST_DF_PRED,
+                                         'y': y,
+                                         'x_list': x_list,
+                                         '_df_test': df_new_test_one_dept,
+                                         '_df_train': df_train_one_dept
+                                         })
+                
+                list_thread.append(_thread)
+                
+            else:
+                reg = Lasso(alpha=0.001, normalize=True, max_iter=10000).fit(df_train_one_dept[x_list], y)
+                df_new_test_one_dept.loc[:, 'Weekly_Pred'] = reg.predict(df_new_test_one_dept[x_list])
+                LIST_DF_PRED.append(df_new_test_one_dept)
+                
+                if show_training_error:
+                    df_train_one_dept['Weekly_Pred'] = reg.predict(df_train_one_dept[x_list])
+                    
+                    training_error = eva_error(
+                        next_fold_=df_train_one_dept[['Date', 'Store', 'Dept', 'Weekly_Sales', 'IsHoliday']],
+                        test_pred_=df_train_one_dept[['Date', 'Store', 'Dept', 'Weekly_Pred', 'IsHoliday']]
+                    )
+                    logging.info(f'dept {dept} and store {store}: training_error: {training_error}')
+        
+    if list_thread:
+        start_multi_threading(list_thread, max_threads=thread_n)
+        
+    test_pred = pd.concat(LIST_DF_PRED)[['Date', 'Store', 'Dept', 'Weekly_Pred', 'IsHoliday']]
+    
+    return new_train, test_pred
+
+
+def set_logging(level=10,
+                path=None):
+    format = '%(levelname)s-%(name)s-%(funcName)s:\n %(message)s'
+    
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    
+    if path:
+        logging.basicConfig(level=level, format=format, filename=path)
+    else:
+        logging.basicConfig(level=level, format=format)
+
+
+warnings.simplefilter(action="ignore")
 
 # use last year to predict
 
@@ -13,177 +231,35 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_colwidth', 40)
 
-logging.basicConfig(level=20)
-
-FOLDER = '/Users/fanyang/Dropbox/uiuc/cs598/UIUC_SPL/UIUC_PSL/Project2/'
-# FOLDER = '/Users/yafa/Dropbox/Library/UIUC_PSL/UIUC_PSL/Project2'
+# FOLDER = '/Users/fanyang/Dropbox/uiuc/cs598/UIUC_SPL/UIUC_PSL/Project2/'
+FOLDER = '/Users/yafa/Dropbox/Library/UIUC_PSL/UIUC_PSL/Project2'
 
 train = pd.read_csv(os.path.join(FOLDER, 'train_ini.csv'), parse_dates=['Date'])
 # df_train.dtypes.value_counts()
 test = pd.read_csv(os.path.join(FOLDER, 'test.csv'), parse_dates=['Date'])
 
-
-# data processing - change date fto datetime format and get year, month and week indicator
-# df_train['Date'] = pd.to_datetime(df_train['Date'])
+START_YEAR = 2010
 
 
-def data_clean(data):
-    data['Week'] = data['Date'].dt.isocalendar().week
-    data['Year'] = data['Date'].dt.isocalendar().year
-    data['Month'] = pd.DatetimeIndex(data['Date']).month
-    # data['Week'] = data.apply(lambda x: x['Week'] - 1 if x['Year'] == 2010 else x['Week'], axis=1)
-    # dummy for holiday
-    holiday_list = [2, 9, 11, 12]
-    for i, m in zip(range(1, 5), holiday_list):
-        name = 'holiday' + str(i)
-        data[name] = data.apply(lambda x: 1 if x['IsHoliday'] == True and x['Month'] == m else 0, axis=1)
-    # dummy for week
-    for i in range(1, 52):
-        name = 'wk' + str(i)
-        data[name] = data.apply(lambda x: 1 if x['Week'] == i else 0, axis=1)
-    return data
-
-
-# def _get_one_hot_encoder_df(_col_name, df_train):
-#     enc = OneHotEncoder(handle_unknown='ignore')
-#     _cate_matrix = [i[[_col_name]] for row_i, i in df_train.iterrows()]
-#     enc.fit(_cate_matrix)
-#     df_hot_code = pd.DataFrame(enc.transform(_cate_matrix).toarray())
-#     df_hot_code.columns = [_col_name + '__' + str(i) for i in df_hot_code.columns]
-#     return {'data_frame': df_hot_code, "encode": enc}
-#
-# def _get_one_hot_encoder_df_test(_col_name, df_test, dict_one_hot_encoder):
-#     enc = dict_one_hot_encoder[_col_name]
-#     _cate_matrix = [i[[_col_name]] for row_i, i in df_test.iterrows()]
-#     df_hot_code = pd.DataFrame(enc.transform(_cate_matrix).toarray())
-#     df_hot_code.columns = [_col_name + '__' + str(i) for i in df_hot_code.columns]
-#     return df_hot_code
-#
-#
-# def transform_category_vars(df):
-#
-#     dict_one_hot_encoder_df = {}
-#     dict_one_hot_encoder = {}
-#     LIST_X = ['Store', 'Dept']
-#     for _col_name in LIST_X:
-#         encode_result = _get_one_hot_encoder_df(_col_name, df)
-#         dict_one_hot_encoder_df[_col_name] = encode_result["data_frame"]
-#         dict_one_hot_encoder[_col_name] = encode_result["encode"]
-#     for _col_name in LIST_X:
-#         # del df[_col_name]
-#         for new_col in dict_one_hot_encoder_df[_col_name]:
-#             df[new_col] = dict_one_hot_encoder_df[_col_name][new_col]
-#
-#     return (df, dict_one_hot_encoder)
-#
-#
-# def transform_category_vars_test(df_test,  dict_one_hot_encoder):
-#     LIST_X = ['Store', 'Dept']
-#     dict_one_hot_encoder_df = {}
-#     # dict_one_hot_encoder = {}
-#     for _col_name in LIST_X:
-#         dict_one_hot_encoder_df[_col_name] = _get_one_hot_encoder_df_test(_col_name, df_test, dict_one_hot_encoder)
-#
-#
-#     for _col_name in LIST_X:
-#         # del df_test[_col_name]
-#         for new_col in dict_one_hot_encoder_df[_col_name]:
-#             df_test[new_col] = dict_one_hot_encoder_df[_col_name][new_col]
-#
-#     return df_test
-# df_train_numeric, dict_one_hot_encoder = transform_category_vars(df_train)
-# df_test_numeric = transform_category_vars_test(df_test, dict_one_hot_encoder)
-
-# x_list = [col for col in df_train_numeric.columns if col not in ['Date', 'Store', 'Dept', 'Weekly_Sales', 'IsHoliday', 'Week', 'Month']]
-# y = df_train_numeric['Weekly_Sales']
-# reg = LinearRegression().fit(df_train_numeric[x_list], y)
-# df_test_numeric['Weekly_Pred'] = reg.predict(df_test_numeric[x_list])
-# X = df_train_numeric[x_list]
-#
-# lasso = linear_model.Lasso(fit_intercept=True,normalize=True,random_state=0, max_iter=10000)
-# alphas = np.logspace(-6, 0.1, 20)
-#
-# tuned_parameters = [{'alpha': alphas}]
-# n_folds = 5
-# clf = GridSearchCV(lasso, tuned_parameters, cv=n_folds, refit=False, scoring='neg_mean_squared_error')
-# clf.fit(X , y)
-# # clf.cv_results_['mean_test_score']
-# best_alpha = clf.best_params_['alpha']
-# print(best_alpha)
-
-
-df_train = data_clean(train)
-df_test = data_clean(test)
-t = 1
-
-delta_m = 2 * (t - 1)
-start_date = pd.Timestamp(datetime.date(2011, 3, 1) + relativedelta(months=delta_m))
-end_date = pd.Timestamp(datetime.date(2011, 5, 1) + relativedelta(months=delta_m))
-mask = (df_test['Date'] >= start_date) & (df_test['Date'] < end_date)
-current_test = df_test[mask]
-df_test['Weekly_Pred'] = 0.0
-
-dic_missing = {}
-
-store_list =  current_test['Store'].unique().tolist()
-
-for store in store_list:
-    for dept in current_test[current_test['Store'] == store]['Dept'].unique().tolist():
-        mask = (df_train['Store'] == store) & (df_train['Dept'] == dept)
-        current_train = df_train[mask]
-        if current_train.shape[0] > 0:
-            y = current_train['Weekly_Sales']
-            x_list = [col for col in df_train.columns if col not in ['Date', 'Store', 'Dept', 'Weekly_Sales', 'IsHoliday', 'Week', 'Month']]
-            X = current_train[x_list]
-            reg = LinearRegression().fit(X, y)
-            test_mask = (df_test['Store'] == store) & (df_test['Dept'] == dept)
-            tmp_test = df_test[test_mask]
-            df_test.loc[test_mask, 'Weekly_Pred'] = reg.predict(tmp_test[x_list])
-        else:
-            if store not in dic_missing.keys():
-                dic_missing[store] = [dept]
-            else:
-                dic_missing[store].append(dept)
-
-
-
-
-def mypredict(train, test, next_fold, t):
-    clean_train = data_clean(train)
-    clean_test = data_clean(test)
-    
-    if t > 1:
-        clean_next_fold = data_clean(next_fold)
-        clean_train = pd.concat([train, clean_next_fold], ignore_index=True)
-    
-    clean_test['Last_Year'] = clean_test['Year'] - 1
-    
-    test_pred = clean_test.merge(clean_train,
-                                 left_on=['Store', 'Dept', 'Last_Year', 'Week'],
-                                 right_on=['Store', 'Dept', 'Year', 'Week'],
-                                 # indicator=True,
-                                 how='left',
-                                 suffixes=('', '_pre'))
-    test_pred = test_pred.rename(columns={'Weekly_Sales': 'Weekly_Pred'})
-    
-    return (clean_train, test_pred)
-
+set_logging(level=20)
 
 n_folds = 10
 next_fold = None
 wae = []
 
+target_fold = 5
+
 # time-series CV
 for t in range(1, n_folds + 1):
     print(f'Fold{t}...')
-    
+    # t = 1
     # *** THIS IS YOUR PREDICTION FUNCTION ***
-    train, test_pred = mypredict(train, test, next_fold, t)
+    train, test_pred = mypredict(train, test, next_fold, t, target_fold=target_fold)
     
     # Load fold file
     # You should add this to your training data in the next call to mypredict()
     fold_file = 'fold_{t}.csv'.format(t=t)
-    next_fold = pd.read_csv(FOLDER + fold_file, parse_dates=['Date'])
+    next_fold = pd.read_csv(os.path.join(FOLDER, fold_file), parse_dates=['Date'])
     
     # extract predictions matching up to the current fold
     scoring_df = next_fold.merge(test_pred, on=['Date', 'Store', 'Dept'], how='left', indicator=True)
@@ -193,17 +269,14 @@ for t in range(1, n_folds + 1):
     actuals = scoring_df['Weekly_Sales'].to_numpy()
     preds = scoring_df['Weekly_Pred'].fillna(0).to_numpy()
     
-    wae.append((np.sum(weights * np.abs(actuals - preds)) / np.sum(weights)).item())
+    weighted_error = np.sum(weights * np.abs(actuals - preds)) / np.sum(weights).item()
+    
+    print(f"error: {weighted_error}")
+    
+    wae.append(weighted_error)
+    
+    if target_fold is not None and t == target_fold:
+        break
 
 print(wae)
 print(sum(wae) / len(wae))
-# mypredict requirement:
-# https://piazza.com/class/kjvsp15j2g07ac?cid=258
-
-# mask = (train_raw['Date_fmt'] >= '2010-02-01') & (train_raw['Date_fmt'] <'2011-03-01')
-# df_train = train_raw.loc[mask]
-# df_test = train_raw.loc[~mask]
-
-# save to csv file
-# df_train.to_csv('train_ini.csv', index =False)
-# df_test.loc[:, df_test.columns != 'Weekly_Sales'].to_csv('test.csv', index = False)
