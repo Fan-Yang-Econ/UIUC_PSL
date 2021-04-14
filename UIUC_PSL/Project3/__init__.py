1432530b629ff2c38c5b6a47881bdd75cc5584e8
yafa <yafa@amazon.com>
2021-04-14 02:01:59 -0700

    add project 3 and 4

---
+++ UIUC_PSL/Project3/__init__.py
@@ -0,0 +1,28 @@
+
+# intro:
+# https://liangfgithub.github.io/S21/Project3_S21.nb.html
+
+# data analysis:
+# https://liangfgithub.github.io/Rcode_W13_Movie_EDA.nb.html
+
+# Project #3: Sentiment Analysis
+# Graded Assignment
+# --
+# Apr 26
+# 9:59 PM PDT
+# 12%
+# --
+# Project #4: Movie Recommendation
+# Graded Assignment
+# --
+# May 9
+# 9:59 PM PDT
+# 0%
+# --
+# Project #4: Movie Recommendation
+# Graded Assignment
+# --
+# May 9
+# 9:59 PM PDT
+# 12%
+# --
---
+++ UIUC_PSL/Project4/__init__.py
@@ -0,0 +1,4 @@
+# intro:
+# https://piazza.com/class/kjvsp15j2g07ac?cid=398
+
+#
