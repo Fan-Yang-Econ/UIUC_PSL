c8fafbc1c30f7be785e3af909e1e89d54cfe27c0
yafa <yafa@amazon.com>
2021-04-16 18:34:40 -0700

    add simple_model_with_one_word.py

--- UIUC_PSL/Project3/construct_vocab.py
+++ UIUC_PSL/Project3/construct_vocab.py
@@ -32,17 +32,15 @@ df = pd.read_csv(os.path.join(FOLDER, 'alldata.tsv'), sep='\t')
 N = len(df)
 
 DICT_VALID_WORDS = {}
-for review in df['review'].tolist():
+for count_i, review in enumerate(df['review'].tolist()):
+    logging.info(f'process review {count_i}')
+    
     for token in nlp(review):
         if token.ent_type_ == '' and not token.is_stop and token.is_alpha and token.pos_ != 'PUNCT' and len(token.text) > 1:
             DICT_VALID_WORDS[token.text] = {
                 'lemma': token.lemma_,
                 'token.pos_': token.pos_
             }
-
-for i in list(DICT_VALID_WORDS.keys()):
-    if DICT_VALID_WORDS[i]['token.pos_'] == 'PROPN':
-        del DICT_VALID_WORDS[i]
 
 DICT_LEMMA = {}
 for word in list(DICT_VALID_WORDS.keys()):
@@ -77,7 +75,6 @@ for word in list(DICT_VALID_WORDS.keys()):
         'sentiment-1 : word-0': (~series_has_lemma & df['sentiment']).sum(),
     }
 
-
 list_confusion_matrix = []
 list_lemma = []
 list_words = []
@@ -85,19 +82,18 @@ for lemma in DICT_LEMMA:
     list_lemma.append(lemma)
     list_confusion_matrix.append(DICT_LEMMA[lemma]['correlation'])
     list_words.append(DICT_LEMMA[lemma]['text_set'])
-    
+
 df_confusion_matrix = pd.DataFrame(list_confusion_matrix)
 df_confusion_matrix['lemma'] = list_lemma
 df_confusion_matrix['word_set'] = list_words
 
-df_confusion_matrix['accuracy'] = (df_confusion_matrix['sentiment-1 : word-1'] + df_confusion_matrix['sentiment-0 : word-0']).apply(lambda x: max(x, N-x))
-
+df_confusion_matrix['accuracy'] = (df_confusion_matrix['sentiment-1 : word-1'] + df_confusion_matrix['sentiment-0 : word-0']).apply(
+    lambda x: max(x, N - x))
 
 df_confusion_matrix = df_confusion_matrix.sort_values('accuracy', ascending=False)
 df_confusion_matrix = df_confusion_matrix[0:5000]
 
 df_confusion_matrix['word_set'] = df_confusion_matrix['word_set'].apply(lambda word_set: set([i.lower() for i in word_set]))
-df_confusion_matrix['word_set'].apply(lambda x: len(x) ).sum()
+df_confusion_matrix['word_set'].apply(lambda x: len(x)).sum()
 
 df_confusion_matrix.to_csv(os.path.join(FOLDER, 'df_confusion_matrix.csv'), index=False)
-
