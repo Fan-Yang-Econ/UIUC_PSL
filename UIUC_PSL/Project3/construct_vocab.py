65a4085dd4faa988203f97513e8664189ba506f3
yafa <yafa@amazon.com>
2021-04-25 16:35:28 -0700

    tried complex method.. did not work

--- UIUC_PSL/Project3/construct_vocab.py
+++ UIUC_PSL/Project3/construct_vocab.py
@@ -1,8 +1,11 @@
 import logging
 import os
+import re
+import ast
 
 import spacy
 import pandas as pd
+import numpy as np
 
 FOLDER = '/Users/yafa/Dropbox/Library/UIUC_PSL/UIUC_PSL/Project3/'
 
@@ -14,10 +17,10 @@ pd.set_option('display.max_colwidth', 40)
 def set_logging(level=10,
                 path=None):
     format = '%(levelname)s-%(name)s-%(funcName)s:\n %(message)s'
-    
+
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
-    
+
     if path:
         logging.basicConfig(level=level, format=format, filename=path)
     else:
@@ -31,69 +34,177 @@ nlp = spacy.load("en_core_web_sm")
 df = pd.read_csv(os.path.join(FOLDER, 'alldata.tsv'), sep='\t')
 N = len(df)
 
-DICT_VALID_WORDS = {}
+list_nlp = []
 for count_i, review in enumerate(df['review'].tolist()):
     logging.info(f'process review {count_i}')
-    
-    for token in nlp(review):
+    list_nlp.append(nlp(review))
+
+import pickle
+with open(os.path.abspath(f'/Users/yafa/Downloads/list_nlp.spacy'), '+wb') as f:
+    if len(list_nlp) == N:
+        pickle.dump(list_nlp, f)
+
+#
+#
+import pickle
+list_nlp = []
+with open(f'/Users/yafa/Downloads/list_nlp.spacy', '+rb') as f:
+    list_nlp2 = pickle.load(f)
+
+len(list_nlp2)
+
+assert len(list_nlp) == N
+
+MEANINGFUL_2_GRAM_set = {'ADJ', 'ADV', 'NOUN', 'PART'}
+DICT_VALID_WORDS = {}
+
+for count_review, review in enumerate(list_nlp):
+    list_two_gram_i = []
+
+    last_token = None
+    for count_i, token in enumerate(review):
+
         if token.ent_type_ == '' and not token.is_stop and token.is_alpha and token.pos_ != 'PUNCT' and len(token.text) > 1:
-            DICT_VALID_WORDS[token.text] = {
-                'lemma': token.lemma_,
-                'token.pos_': token.pos_
-            }
+            lemma_key = (token.lemma_, token.pos_)
+            if lemma_key not in DICT_VALID_WORDS:
+                DICT_VALID_WORDS[lemma_key] = []
 
-DICT_LEMMA = {}
-for word in list(DICT_VALID_WORDS.keys()):
-    lemma = DICT_VALID_WORDS[word]['lemma']
-    if lemma not in DICT_LEMMA:
-        DICT_LEMMA[lemma] = {'text_set': set()}
-    DICT_LEMMA[lemma]['text_set'].add(word)
+            DICT_VALID_WORDS[lemma_key].append(count_review)
 
-len(DICT_LEMMA)
+        if count_i > 0:
 
+            if (last_token.is_alpha and token.is_alpha) and \
+                    (not last_token.is_stop and not token.is_stop) and \
+                    {last_token.pos_, token.pos_}.issubset(MEANINGFUL_2_GRAM_set):
+                two_gram = f'{last_token.lemma_} {token.lemma_}'
 
-def _in_sentence(set_words, sentence):
-    for word in set_words:
-        if word in sentence:
-            return True
-    else:
-        return False
+                if last_token.pos_ == 'NOUN' and token.pos_ == 'ADV':
+                    print(count_review, two_gram)
+                else:
+                    if two_gram not in DICT_VALID_WORDS:
+                        DICT_VALID_WORDS[two_gram] = []
+                    DICT_VALID_WORDS[two_gram].append(count_review)
 
+        last_token = token
 
-for word in list(DICT_VALID_WORDS.keys()):
-    logging.info(f'Evaluate correlation for word `{word}`')
-    lemma = DICT_VALID_WORDS[word]['lemma']
-    if 'correlation' in DICT_LEMMA[lemma]:
+len(DICT_VALID_WORDS)
+
+list_only_one_frq = []
+DICT_VALID_WORD_WITH_SENTIMENT = {}
+
+for i in list(DICT_VALID_WORDS):
+    freq = len(DICT_VALID_WORDS[i])
+    if freq == 1:
+        list_only_one_frq.append(i)
+        del DICT_VALID_WORDS[i]
         continue
-    series_has_lemma = df['review'].apply(lambda sentence: _in_sentence(set_words=DICT_LEMMA[lemma]['text_set'], sentence=sentence))
-    if series_has_lemma.empty:
-        raise ValueError(f'Cannot find word {word}')
-    
-    DICT_LEMMA[lemma]['correlation'] = {
-        'sentiment-1 : word-1': (series_has_lemma & df['sentiment']).sum(),
-        'sentiment-0 : word-0': (~series_has_lemma & ~df['sentiment']).sum(),
-        'sentiment-1 : word-0': (~series_has_lemma & df['sentiment']).sum(),
+
+    sentiment_series = df['sentiment'][DICT_VALID_WORDS[i]]
+    DICT_VALID_WORD_WITH_SENTIMENT[i] = {
+        1: (sentiment_series == 1).sum(),
+        0: (sentiment_series == 0).sum()
     }
 
-list_confusion_matrix = []
-list_lemma = []
-list_words = []
-for lemma in DICT_LEMMA:
-    list_lemma.append(lemma)
-    list_confusion_matrix.append(DICT_LEMMA[lemma]['correlation'])
-    list_words.append(DICT_LEMMA[lemma]['text_set'])
+for i in list(DICT_VALID_WORDS):
+    if DICT_VALID_WORD_WITH_SENTIMENT[i][1] == DICT_VALID_WORD_WITH_SENTIMENT[i][0]:
+        del DICT_VALID_WORDS[i]
 
-df_confusion_matrix = pd.DataFrame(list_confusion_matrix)
-df_confusion_matrix['lemma'] = list_lemma
-df_confusion_matrix['word_set'] = list_words
+    count_ = (DICT_VALID_WORD_WITH_SENTIMENT[i][1] + DICT_VALID_WORD_WITH_SENTIMENT[i][0])
+    DICT_VALID_WORD_WITH_SENTIMENT[i]['diff'] = max(DICT_VALID_WORD_WITH_SENTIMENT[i][1], DICT_VALID_WORD_WITH_SENTIMENT[i][0]) / count_
+    DICT_VALID_WORD_WITH_SENTIMENT[i]['count'] = count_
 
-df_confusion_matrix['accuracy'] = (df_confusion_matrix['sentiment-1 : word-1'] + df_confusion_matrix['sentiment-0 : word-0']).apply(
-    lambda x: max(x, N - x))
+list_diff = []
+list_count_word = []
+valid_words = list(DICT_VALID_WORDS)
+for i in valid_words:
+    list_diff.append(DICT_VALID_WORD_WITH_SENTIMENT[i]['diff'])
+    list_count_word.append(DICT_VALID_WORD_WITH_SENTIMENT[i]['count'])
 
-df_confusion_matrix = df_confusion_matrix.sort_values('accuracy', ascending=False)
-df_confusion_matrix = df_confusion_matrix[0:5000]
+df_diff = pd.DataFrame({'diff': list_diff, 'word': valid_words, 'count': list_count_word})
+df_diff = df_diff.sort_values('diff', ascending=False)
 
-df_confusion_matrix['word_set'] = df_confusion_matrix['word_set'].apply(lambda word_set: set([i.lower() for i in word_set]))
-df_confusion_matrix['word_set'].apply(lambda x: len(x)).sum()
+# not (
+#             re.compile('ing$').findall(x[0]) + re.compile('ness$').findall(x[0]) + re.compile('y$').findall(x[0]) + re.compile('y$').findall(x[0])
+#     )
 
-df_confusion_matrix.to_csv(os.path.join(FOLDER, 'df_confusion_matrix.csv'), index=False)
+
+words_with_same_reviews_id = []
+
+for count_i, df_diff_i in df_diff.groupby('count'):
+    list_sets = []
+    for word in df_diff_i['word'].tolist():
+        new_set = set(DICT_VALID_WORDS[word])
+        if new_set in list_sets:
+            words_with_same_reviews_id.append(word)
+        else:
+            list_sets.append(new_set)
+
+df_diff = df_diff[~df_diff['word'].isin(words_with_same_reviews_id)]
+
+print(len(df_diff))
+print((df_diff['count'] < 10).sum())
+
+df_diff_small = df_diff[df_diff['count'] < 10]
+df_diff_big = df_diff[df_diff['count'] >= 8]
+
+df_features = pd.read_csv('/Users/yafa/Dropbox/Library/UIUC_PSL/UIUC_PSL/Project3/df_features.csv')
+df_features = df_features.sort_values('importance', ascending=False)
+df_features_1000 = df_features[0:2000]
+df_features_1000['col_name'] = df_features_1000['col_name'].apply(lambda x: ast.literal_eval(x) if '(' in x else x)
+
+list_features = df_features_1000['col_name'].tolist()
+len(list_features)
+
+def create_feature_df(list_features):
+    DICT_VALID_WORDS_AS_FEATURE = {}
+    for i in list_features:
+        zero_list = np.repeat(0, N)
+        for review_id in DICT_VALID_WORDS[i]:
+            zero_list[review_id] += 1
+        DICT_VALID_WORDS_AS_FEATURE[i] = zero_list
+
+    return pd.DataFrame(DICT_VALID_WORDS_AS_FEATURE)
+
+df_feature = create_feature_df(list_features)
+
+len(df_feature.columns)
+
+
+len(df_diff_big)
+
+DICT_REVIEW_ID_REPRESENTED = {}
+for word in list_features:
+    for review_id in DICT_VALID_WORDS[word]:
+        DICT_REVIEW_ID_REPRESENTED[review_id] = DICT_REVIEW_ID_REPRESENTED.get(review_id, 0) + 1
+
+len(DICT_REVIEW_ID_REPRESENTED)
+
+
+
+
+
+del globals()['clf']
+
+
+
+len(df_diff[df_diff['count'] >= 1000])
+len(df_diff[df_diff['count'] >= 500])
+len(df_diff[df_diff['count'] >= 100])
+len(df_diff[df_diff['count'] >= 50])
+len(df_diff[df_diff['count'] >= 10])
+len(df_diff[df_diff['count'] < 10])
+
+len(words_with_same_reviews_id)
+
+single_nouns = df_diff[df_diff['word'].apply(
+    lambda x:
+    type(x) is tuple and x[1] == 'NOUN'
+)]['word'].apply(lambda x: x[0]).tolist()
+
+len(single_nouns)
+two_grams = df_diff[df_diff['word'].apply(lambda x: type(x) is str)]['word'].tolist()
+two_grams_list = set(' '.join(two_grams).split(' '))
+
+
+df_feature.to_csv(os.path.join(FOLDER, 'feature.csv'))
+df_corr = df_feature.corr(method='pearson')
--- UIUC_PSL/Project3/simple_model_with_one_word.py
+++ UIUC_PSL/Project3/simple_model_with_one_word.py
@@ -17,10 +17,10 @@ pd.set_option('display.max_colwidth', 40)
 def set_logging(level=10,
                 path=None):
     format = '%(levelname)s-%(name)s-%(funcName)s:\n %(message)s'
-    
+
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
-    
+
     if path:
         logging.basicConfig(level=level, format=format, filename=path)
     else:
@@ -29,15 +29,14 @@ def set_logging(level=10,
 
 set_logging(20)
 
-df = pd.read_csv(os.path.join(FOLDER, 'alldata.tsv'), sep='\t')
+d_one_fold_train = pd.read_csv(os.path.join(FOLDER, 'train_0.csv'))
 
 df_confusion_matrix = pd.read_csv(os.path.join(FOLDER, 'df_confusion_matrix.csv'))
-df_confusion_matrix = df_confusion_matrix[0:2000]
+# df_confusion_matrix = df_confusion_matrix[0:2000]
 
 N = len(df)
 
 nlp = spacy.load("en_core_web_sm")
-
 
 # add review ids to each lemma
 DICT_LEMMA = {}
@@ -52,7 +51,7 @@ for index_i, row_i in df_confusion_matrix.iterrows():
 list_reviews_with_lemma = {}
 for count_i, row_i in df.iterrows():
     logging.info(f'process review {count_i}')
-    
+
     set_lemma = set()
     for token in nlp(row_i['review']):
         if token.lemma_ in DICT_LEMMA:
@@ -70,5 +69,222 @@ x_variables = [i for i in df.columns if 'lemma-' in i]
 lasso_model.fit(X=df[x_variables], y=df['sentiment'])
 prediction = lasso_model.predict_proba(X=df[x_variables])
 
+##====
+
+N = 25000
+model = LogisticRegression(penalty='l1', solver='liblinear', C=0.1)
+model.fit(X=df_feature.loc[0:N, selected_list_var], y=df.loc[0:N, 'sentiment'])
+prediction = model.predict_proba(X=df_feature.loc[0:N, selected_list_var])
+metrics.roc_auc_score(df.loc[0:N, 'sentiment'], prediction[:, 1])
+
+df_model_coef = pd.DataFrame(model.coef_.reshape(-1, ), columns=['coef'])
+lasso_var = df_model_coef[abs(df_model_coef['coef']) > 0].index.tolist()
+len(lasso_var)
+
+selected_list_var = pd.Series(df_feature.columns)[lasso_var].tolist()
+len(selected_list_var)
+
+len(df_feature.columns)
+
+# select best features
+myvocab = features.loc[lasso_var, :]['features'].tolist()
+
+len(myvocab)
+
 # AUC
 metrics.roc_auc_score(df['sentiment'], prediction[:, 1])
+
+df_available = df_feature.loc[df_missing.index, selected_list_var].sum(axis=1)
+
+df_missing = df[df['sentiment'] != pd.Series(prediction[:, 1] > 0.5).apply(lambda x: 1 if x else 0)]
+
+SELECT_FEATURE_IND = False
+
+
+def two_step_predict(df_x_train, training_y, df_test_x, y_test):
+    """
+
+    :param df_x_train:
+        df_x_train = df_feature
+
+        df_x_train = df_feature[list_features]
+        df_x_train = df_feature[df_features_500['col_name']]
+
+        df_x_train = df_feature_small[df_feature_small.index.isin(review_id_with_wrong_prediciton)]
+
+        len(df_feature.column)
+
+    :param training_y:
+        training_y = df['sentiment']
+
+        training_y = df[df['id'].isin(review_id_with_wrong_prediciton)]['sentiment']
+    :param df_test_x:
+    :param y_test:
+    :return:
+    """
+
+    from sklearn.ensemble import GradientBoostingClassifier
+    SELECT_FEATURE_IND = 0
+
+    for iteration in [180, 250, 300, 400, 500, 800]:
+        for max_depth in [10, 20]:
+
+            learning_rate = 0.1
+            iteration = 1000
+            max_depth = 20
+            SELECT_FEATURE_IND = 1
+
+            clf = GradientBoostingClassifier(
+                n_estimators=iteration,
+                learning_rate=learning_rate,
+                max_depth=max_depth,
+                subsample=0.5 if SELECT_FEATURE_IND else 1,
+                verbose=True,
+                random_state=0).fit(
+                df_x_train,
+                training_y)
+
+            prediction_test = clf.predict_proba(X=df_test_x)
+            print(f'test error for iteration {iteration} and max_depth {max_depth}')
+            print(metrics.roc_auc_score(y_test, prediction_test[:, 1]))
+
+            prediction_training = clf.predict_proba(X=df_x_train)
+            print('training_error')
+            print(metrics.roc_auc_score(training_y, prediction_training[:, 1]))
+
+
+    clf = LogisticRegression(fit_intercept=False, penalty='l1', solver='liblinear')
+    clf.fit(X=df_x_train, y=training_y)
+
+    print(f'test error for iteration in logistic')
+    prediction_test = clf.predict_proba(X=df_test_x)
+    print(metrics.roc_auc_score(y_test, prediction_test[:, 1]))
+
+    df_model_coef = pd.DataFrame(clf.coef_.reshape(-1, ), columns=['coef'])
+    lasso_var = df_model_coef[abs(pd.DataFrame(clf.coef_.reshape(-1, ), columns=['coef'])['coef']) > 0].index.tolist()
+    selected_list_var = pd.Series(df_x_train.columns)[lasso_var].tolist()
+
+    print(len(selected_list_var))
+
+
+
+    if SELECT_FEATURE_IND:
+        df_features = pd.DataFrame({'col_name': df_x_train.columns, 'importance': clf.feature_importances_})
+        df_features = df_features.sort_values(['importance'], ascending=False)
+        df_features.to_csv('/Users/yafa/Dropbox/Library/UIUC_PSL/UIUC_PSL/Project3/df_features.csv')
+
+        df_features = df_features[df_features['importance'] > 0]
+        df_features.iloc[10]
+
+
+        from pprint import pprint
+        pprint(df_features[400:500]['col_name'].tolist())
+
+        df_features = pd.read_csv('/tmp/df_features.csv')
+        df_features['col_name'] = df_features['col_name'].apply(lambda x: ast.literal_eval(x) if '(' in x  else x)
+
+        pprint(df_features[100:1000][df_features[100:1000]['col_name'].apply(lambda x: type(x) == tuple and x[1] in ['ADJ', 'ADV'])]['col_name'].tolist())
+
+        list_to_not_include = df_features[100:1000][df_features[100:1000]['col_name'].apply(lambda x: type(x) == tuple and x[1] not in ['ADJ', 'ADV'])]['col_name'].tolist()
+        len(list_to_not_include)
+
+        df_features_500 = df_features[0:500][~df_features[0:500]['col_name'].isin(list_to_not_include)]
+
+        pprint(df_features[100:1000][df_features[100:1000]['col_name'].apply(lambda x: type(x) != tuple )]['col_name'].tolist())
+
+
+
+        (df_diff[df_diff['word'].isin(feasures_1000)]['count'] <= 50).sum()
+
+
+        feasures_1000 = df_features[0:2000]['col_name'].tolist()
+        len(feasures_1000)
+
+
+
+        prediction_training
+
+        boolean_wrong = (training_y != pd.Series(prediction_training[:, 1] ).apply(lambda x: 1 if x else 0)).tolist()
+        df_missing = training_y[boolean_wrong]
+
+        review_id_with_wrong_prediciton = set(df_missing.index.tolist())
+
+        review_id_with_wrong_prediciton
+
+        list_word_in_wrong_reviews = []
+        for i in df_diff_small['word'].tolist():
+            if set(DICT_VALID_WORDS[i]).intersection(review_id_with_wrong_prediciton):
+                list_word_in_wrong_reviews.append(i)
+
+        list_word_in_wrong_reviews[1]
+
+        len(list_word_in_wrong_reviews)
+        len(df_diff_small)
+        len(review_id_with_wrong_prediciton)
+
+        df_diff_small_wrong = df_diff_small[df_diff_small['word'].isin(list_word_in_wrong_reviews)]
+        df_diff_small_wrong = df_diff_small_wrong[df_diff_small_wrong['diff'] == 1]
+
+        df_diff_small_wrong = df_diff_small_wrong.sort_values('count', ascending=False)
+
+        final_features = df_features[0:(1000 - 133)]['col_name'].tolist() + df_diff_small_wrong[df_diff_small_wrong['count'] >= 8]['word'].tolist()
+        len(final_features)
+
+    # feature_importances_ndarray of shape (n_features,)
+    # The impurity-based feature importances.
+    #
+    # oob_improvement_ndarray of shape (n_estimators,)
+
+    # clf = LogisticRegression(solver='liblinear')
+    # clf.fit(X=df_x_train, y=training_y)
+    prediction_training = clf.predict_proba(X=df_x_train)
+    print(metrics.roc_auc_score(training_y, prediction_training[:, 1]))
+
+    # Run the Logistic model with Lasso
+
+
+    prediction = clf.predict_proba(X=df_x_train)
+    print(metrics.roc_auc_score(training_y, prediction[:, 1]))
+
+    boolean_wrong = (training_y != pd.Series(prediction[:, 1] > 0.5).apply(lambda x: 1 if x else 0)).tolist()
+    df_missing = training_y[boolean_wrong]
+    df_feature2 = df_x_train.loc[boolean_wrong]
+
+    model_2 = LogisticRegression(solver='liblinear')
+    model_2.fit(X=df_feature2, y=df_missing)
+    df_model_coef = pd.DataFrame(model_2.coef_.reshape(-1, ), columns=['coef'])
+    lasso_var = df_model_coef[abs(pd.DataFrame(model_2.coef_.reshape(-1, ), columns=['coef'])['coef']) > 0].index.tolist()
+    selected_list_var_2 = pd.Series(df_x_train.columns)[lasso_var].tolist()
+
+    len(pd.Series(selected_list_var_2 + selected_list_var).unique().tolist())
+
+    LIST_VARS = pd.Series(selected_list_var_2 + selected_list_var).unique().tolist()
+
+    prediction_2 = model_2.predict_proba(X=df_feature2)
+
+    prediction_series = deepcopy(prediction[:, 1])
+    prediction_series[df_missing['sentiment'].index] = prediction_2[:, 1]
+
+    metrics.roc_auc_score(y_test, prediction_series)
+
+
+from copy import deepcopy
+
+df_one_fold_train = pd.read_csv(os.path.join(FOLDER, 'train_1.csv'))
+df_one_fold_test = pd.read_csv(os.path.join(FOLDER, 'test_1.csv'))
+df_one_fold_test_y = pd.read_csv(os.path.join(FOLDER, 'test_y_1.csv'))
+
+df_x_train = df_feature[df['id'].isin(df_one_fold_train['id'])][list_features]
+training_y = df_one_fold_train['sentiment']
+assert len(df_x_train) == len(training_y) == len(df_one_fold_train)
+
+len(df_x_train.columns)
+
+df_test_x = df_feature[df['id'].isin(df_one_fold_test_y['id'])][list_features]
+assert len(df_test_x) == len(df_one_fold_test_y)
+y_test = pd.Series(df_one_fold_test_y['sentiment'])
+
+two_step_predict(df_x_train, training_y, df_test_x, y_test)
+
+test = df_test_x.iloc[0:4]
+test.loc[pd.Series([True, False, False, False])]
